5cf04a129dee7b612c8f0c6e9b3e8cdbec7e02f9
Armen <mench256@gmail.com>
2025-09-01 22:23:52 +0300

    Updates

--- _app/immutable/chunks/BHpf1Q8C.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_179jqyh?.base??"/trainer",a=globalThis.__sveltekit_179jqyh?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/OhoGJU6O.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_8hmq28?.base??"/trainer",a=globalThis.__sveltekit_8hmq28?.assets??s;export{a,s as b};
